f554a222eac95f385fce0c25dafa845491e738fb
enneyehc97 <cheyenneeeloh@gmail.com>
2021-10-29 19:08:31 +0800

    update confirmation page

---
+++ confirmation.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Confirmation | Scenema</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./css/scenema.css">
+    <link rel="stylesheet" href="./css/confirmation.css">
+    <script type="text/javascript" src="jobs-validation.js"></script>
+</head>
+
+<body>
+    <div id="wrapper">
+        <header>
+            <img id="navilogo" src="assets/logo.png">
+        </header>
+
+    <div id="content">
+        <table id="progress">
+            <tr>
+                <td id="smalltitle">SELECT SEATS</td>
+                <td id="selected">CONFIRMATION</td>
+                <td id="smalltitle">PAYMENT</td>
+                <td id="smalltitle">SUCCESSFUL BOOKING</td>
+            </tr>
+        </table>
+            <h1 id="title">CONFIRMATION</h1>
+    </div>
+        <div id="confirmationslip">
+            <p>confirmation slip</p>
+        </div>
+
+    <div id="homebuttons">
+    <button id="confirmationbutt">GO BACK</button>
+    <button id="confirmationbutt">PROCEED TO PAYMENT</button>
+    </div>
+    </div>
+</body>
+</html>
